4df8abb50f141fe6fc91160e3ba2a9c47f9677c2
Natalia <ef.nata@gmail.com>
2020-09-23 12:33:19 +0300

    Фильтр в каталоге JS

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
+var currentLocation = window.location;
+
 // Menu
 
 var navMain = document.querySelector('.main-nav');
@@ -86,3 +88,24 @@ filterUnwrap.addEventListener('click', function() {
     filterCountry.classList.add('filter-country--closed');
   }
 });
+
+// Filter travel
+
+function legendToggle() {
+  var fieldsetAll = document.querySelectorAll(".main-filter__set");
+  var addToggleHandler = function (toggle, fieldset) {
+    toggle.addEventListener("click", function () {
+      fieldset.classList.toggle("main-filter__set--active");
+    });
+  };
+  for (var i = 0; i < fieldsetAll.length; i++) {
+    var fieldset = fieldsetAll[i];
+    var toggle = fieldset.querySelector(".main-filter__toggle");
+    addToggleHandler(toggle, fieldset);
+    fieldset.classList.remove("main-filter__set--active");
+  }
+}
+
+if (currentLocation.pathname.lastIndexOf("/catalog.html") >= 0) {
+  legendToggle();
+}
